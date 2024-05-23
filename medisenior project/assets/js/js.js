6b27ef7103f1c9e777da19dcc5330a673081da69
Gabriel Barbosa <gabrielbarbosax123@gmail.com>
2024-05-23 16:09:08 -0300

    assets

---
+++ medisenior project/assets/js/js.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal({reset:true})
+
+ScrollReveal().reveal('.banner-options .banner1', {
+    delay:100,
+    origin: 'left',
+        duration: 500,
+        distance: '100%'
+});
+ScrollReveal().reveal('.banner-options .banner2', {
+    delay:200,
+    origin: 'left',
+        duration: 1000,
+        distance: '100%'
+});
+ScrollReveal().reveal('.banner-options .banner3', {
+    delay:300,
+    origin: 'left',
+        duration: 1500,
+        distance: '100%'
+});
