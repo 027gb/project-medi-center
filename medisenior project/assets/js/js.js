17693b98f5f6c204b16ea3cf166c8747bf22912a
Gabriel Barbosa <gabrielbarbosax123@gmail.com>
2024-05-24 16:16:43 -0300

    update-footer

--- medisenior project/assets/js/js.js
+++ medisenior project/assets/js/js.js
@@ -1,4 +1,4 @@
-window.sr = ScrollReveal({reset:true})
+window.sr = ScrollReveal({reset:false})
 
 ScrollReveal().reveal('.banner-options .banner1', {
     delay:100,
@@ -13,6 +13,25 @@ ScrollReveal().reveal('.banner-options .banner2', {
         distance: '100%'
 });
 ScrollReveal().reveal('.banner-options .banner3', {
+    delay:300,
+    origin: 'left',
+        duration: 1500,
+        distance: '100%'
+});
+
+ScrollReveal().reveal('.footer-options .footer-1', {
+    delay:100,
+    origin: 'left',
+        duration: 500,
+        distance: '100%'
+});
+ScrollReveal().reveal('.footer-options .footer-2', {
+    delay:200,
+    origin: 'left',
+        duration: 1000,
+        distance: '100%'
+});
+ScrollReveal().reveal('.footer-options .footer-3', {
     delay:300,
     origin: 'left',
         duration: 1500,
